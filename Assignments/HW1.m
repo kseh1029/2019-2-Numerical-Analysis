273b4927e16ffe2f494f06ec02a444059e30e8f7
kseh1029 <48405068+kseh1029@users.noreply.github.com>
2019-11-28 00:45:03 +0900

    Add files via upload

---
+++ Assignments/HW1.m
@@ -0,0 +1,9 @@
+% 2017117876 ±è½ÂÇö
+function exp_2017117876(xmin,xmax,data,a,n)
+x = linspace(xmin,xmax,data);
+y = zeros(1,data);
+for ncount=0:1:n
+	y = y + ((-1)^(ncount) * exp(-a) * (x-a).^ncount) / factorial(ncount);
+end
+plot(x,y, 'r--o','linewidth',3);
+end
